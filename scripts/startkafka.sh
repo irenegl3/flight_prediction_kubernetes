e649f98a17a34de549157dd7aaf4a3e6be59d1d9
IRENE <irene.garcialop@alumnos.upm.es>
2020-11-22 17:41:02 +0100

    arrancar kafka y zookeper background

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd kafka_2.12-2.3.0
+./bin/zookeeper-server-start.sh config/zookeeper.properties &
+
+./bin/kafka-server-start.sh config/server.properties &
 
 ./bin/kafka-topics.sh \
     --create \
     --zookeeper localhost:2181 \
     --replication-factor 1 \
     --partitions 1 \
-    --topic flight_delay_classification_request
-
-./bin/kafka-server-start.sh config/server.properties
+    --topic flight_delay_classification_request 
