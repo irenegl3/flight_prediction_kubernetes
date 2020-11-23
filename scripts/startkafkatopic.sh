afd9f29516d587e280f92d2dead34320be6c7bf2
IRENE <irene.garcialop@alumnos.upm.es>
2020-11-23 12:58:10 +0100

    cambio puerto de zookeper expuesto

--- scripts/startkafkatopic.sh
+++ scripts/startkafkatopic.sh
@@ -2,7 +2,7 @@
 
 ./bin/kafka-topics.sh \
     --create \
-    --zookeeper 35.246.2.118:2181 \
+    --zookeeper 35.246.2.118:30181 \
     --replication-factor 1 \
     --partitions 1 \
     --topic flight_delay_classification_request 
