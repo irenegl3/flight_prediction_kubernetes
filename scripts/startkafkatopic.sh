8253147f6c6ea3c0ea9bab511caa301376ad31a2
Belen Balmori <36365130+BBalmori@users.noreply.github.com>
2020-11-29 20:26:50 +0100

    Delete startkafkatopic.sh

--- scripts/startkafkatopic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./bin/kafka-topics.sh \
-    --create \
-    --zookeeper 35.246.2.118:30181 \
-    --replication-factor 1 \
-    --partitions 1 \
-    --topic flight_delay_classification_request 
